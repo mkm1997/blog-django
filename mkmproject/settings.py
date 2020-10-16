c2c7cbf3ecdafc2b3ca5d49c2f648554e359b5a4
Manish <mmanish184@gmail.com>
2020-10-16 22:12:16 +0530

    stuctured the code.

--- mkmproject/settings.py
+++ mkmproject/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djcelery',
-    'corsheaders',
     'rest_framework',
     
 ]
@@ -136,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILLES_DIRS = [STATIC_DIR,]
 LOGIN_REDIRECT_URL = '/user_login/'
